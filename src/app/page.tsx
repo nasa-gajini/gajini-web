9c46246330d57680ce67712bb899a74f3d6edcce
subin7906 <subin7906@gmail.com>
2024-10-05 00:25:16 +0900

    Feat: 이집트경계 geojson 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { MapContainer, FeatureGroup, TileLayer } from "react-leaflet";
+import { useState, useEffect } from "react";
+
 import { LatLngBounds } from "leaflet";
+import { MapContainer, FeatureGroup, TileLayer, GeoJSON } from "react-leaflet";
 import { EditControl } from "react-leaflet-draw";
 import "leaflet/dist/leaflet.css";
 import "leaflet-draw/dist/leaflet.draw.css";
@@ -9,6 +11,15 @@ import "leaflet-draw/dist/leaflet.draw.css";
 const egyptBounds = new LatLngBounds([22.0, 25.0], [31.7, 35.0]);
 
 export default function Home() {
+  const [egyptBorder, setEgyptBorder] = useState();
+
+  useEffect(() => {
+    fetch("/assets/geojson/geoBoundaries-EGY-ADM0.geojson") // public/assets 경로의 GeoJSON 파일
+      .then((response) => response.json())
+      .then((data) => setEgyptBorder(data))
+      .catch((error) => console.error("Error loading GeoJSON:", error));
+  }, []);
+
   return (
     <>
       <h4>Step 1. 농지 영역을 선택해주세요</h4>
@@ -16,8 +27,8 @@ export default function Home() {
       <MapContainer
         center={[26.8206, 30.8025]} // 이집트의 중앙 좌표
         zoom={6}
-        minZoom={5}
-        maxZoom={10}
+        minZoom={6}
+        maxZoom={16}
         maxBounds={egyptBounds}
         maxBoundsViscosity={1.0}
         style={{ height: "80vh" }}
@@ -26,6 +37,13 @@ export default function Home() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://tile.openstreetmap.bzh/br/{z}/{x}/{y}.png"
         />
+
+        {egyptBorder && (
+          <GeoJSON
+            data={egyptBorder}
+            style={{ color: "black", weight: 2, fillOpacity: 0 }}
+          />
+        )}
 
         <FeatureGroup>
           <EditControl
