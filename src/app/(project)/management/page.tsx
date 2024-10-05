2134d02ceed88bd996baa30f14e9a1254106e0c3
subin7906 <subin7906@gmail.com>
2024-10-05 12:20:20 +0900

    Feat: 관리 페이지 생성

--- src/app/(project)/management/page.tsx
+++ src/app/(project)/management/page.tsx
@@ -1,5 +1,51 @@
+"use client";
+
+import { useState, useEffect, useRef } from "react";
+
+import { LatLngBounds, Map as LeafletMap } from "leaflet";
+import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+const egyptBounds = new LatLngBounds([22.0, 25.0], [31.7, 35.0]);
+
 const ManagementPage = () => {
-  return <>dd</>;
+  const [egyptBorder, setEgyptBorder] = useState<GeoJSON.FeatureCollection>();
+
+  const mapRef = useRef<LeafletMap>(null);
+
+  useEffect(() => {
+    fetch("/assets/geojson/geoBoundaries-EGY-ADM0.geojson") // public/assets 경로의 GeoJSON 파일
+      .then((response) => response.json())
+      .then((data) => setEgyptBorder(data))
+      .catch((error) => console.error("Error loading GeoJSON:", error));
+  }, []);
+
+  return (
+    <>
+      <MapContainer
+        ref={mapRef}
+        center={[26.8206, 30.8025]} // 이집트의 중앙 좌표
+        zoom={6.2}
+        minZoom={6.2}
+        maxZoom={16}
+        maxBounds={egyptBounds}
+        maxBoundsViscosity={1.0}
+        style={{ width: "100%", height: "100%" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://tile.openstreetmap.bzh/br/{z}/{x}/{y}.png"
+        />
+
+        {egyptBorder && (
+          <GeoJSON
+            data={egyptBorder}
+            style={{ color: "black", weight: 2, fillOpacity: 0 }}
+          />
+        )}
+      </MapContainer>
+    </>
+  );
 };
 
 export default ManagementPage;
