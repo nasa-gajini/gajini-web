2213800473611c960290f02ddb04fcfa1a4eb1cc
subin7906 <subin7906@gmail.com>
2024-10-06 22:47:03 +0900

    Chore: react query 설치

--- src/app/(project)/layout.tsx
+++ src/app/(project)/layout.tsx
@@ -2,14 +2,20 @@
 
 import { PropsWithChildren } from "react";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import { Box } from "@mui/material";
 import RectangleProvider from "@/providers/Rectangle";
 
 const ProjectLayout = ({ children }: PropsWithChildren) => {
+  const queryClient = new QueryClient();
+
   return (
-    <Box width="100%" height="100vh" display="flex" flexDirection="column">
-      <RectangleProvider>{children}</RectangleProvider>
-    </Box>
+    <QueryClientProvider client={queryClient}>
+      <Box width="100%" height="100vh" display="flex" flexDirection="column">
+        <RectangleProvider>{children}</RectangleProvider>
+      </Box>
+    </QueryClientProvider>
   );
 };
 
